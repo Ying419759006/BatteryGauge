c58bf8ed845fe6cc4f642cd12b74973da3e47201
ZYingYing <zhaoying9006@163.com>
2017-09-04 22:48:18 +0800

    初始化项目, 简介:仪表盘样式进度条

---
+++ HQBatteryGauge-master/HQBatteryGauge/HQBatteryGauge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQBatteryGauge
+//
+//  Created by zhaoy on 2017/7/19.
+//  Copyright © 2017年 zhaoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQBatteryGauge-master/HQBatteryGauge/HQBatteryGauge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQBatteryGauge
+//
+//  Created by zhaoy on 2017/7/19.
+//  Copyright © 2017年 zhaoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
